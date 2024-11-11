ec372fc63d6e159c294389d168168f6c3a9c7283
Ajdin Mehmedović <ayden.diagrampixel@gmail.com>
2024-11-11 14:16:21 +0100

    about page

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -6,7 +6,7 @@ export default function Providers({ children }) {
   return (
     <ThemeProvider enableSystem={true} 
     attribute="class">
-      <div className='dark:bg-zinc-900 dark:text-gray-200 text-gray-400 
+      <div className='dark:bg-zinc-900 dark:text-gray-200 text-gray-700 
       transition-colors duration-300 min-h-screen select-none'>{children}</div>
       </ThemeProvider>
   )
