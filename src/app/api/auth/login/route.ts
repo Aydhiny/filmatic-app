7e36edab2062f8a920e0f68daf93b1ce1dd12ce0
Ajdin Mehmedović <ayden.diagrampixel@gmail.com>
2025-03-04 12:09:57 +0100

    Login functionality completed, bcrypt library used for hashing, by logging in we redirect user to dashboard

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,24 +1,26 @@
 import { NextResponse } from "next/server";
-import { signIn } from "next-auth/react";
+import bcrypt from "bcryptjs";
+import prisma from "../../../../../lib/prisma";
 
 export async function POST(req: Request) {
   const { email, password } = await req.json();
 
   try {
-    const res = await signIn("credentials", {
-      redirect: false,
-      email,
-      password,
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
     });
 
-    console.log(res);
-
-    if (res?.error) {
+    if (!existingUser) {
       return NextResponse.json({ error: "Invalid credentials" }, { status: 400 });
     }
 
-    return NextResponse.json({ message: "Login successful" });
+    const isPasswordValid = await bcrypt.compare(password, existingUser.password);
 
+    if (!isPasswordValid) {
+      return NextResponse.json({ error: "Invalid credentials" }, { status: 400 });
+    }
+
+    return NextResponse.json({ message: "Login successful", user: existingUser });
   } catch (error) {
     console.error("Login error:", error);
     return NextResponse.json({ error: "An error occurred during login" }, { status: 500 });
